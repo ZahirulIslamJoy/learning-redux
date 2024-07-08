e5c29e6df684ac32a137e828e3add0b8bebc810d
ZahirulIslamJoy <neloy1613@gmail.com>
2024-07-08 21:44:42 +0600

    completed counter with redux

--- src/redux/features/counterSlice.tsx
+++ src/redux/features/counterSlice.tsx
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from '@reduxjs/toolkit'
 const initialState = {
     count : 0
 }
@@ -6,6 +7,18 @@ const counterSlice=createSlice({
     name:"counter",
     initialState,
     reducers:{
-
+        increment : (state)=>{
+            state.count +=1
+        },
+        incrementbyValue : (state ,action: PayloadAction<number>)=>{
+            state.count += action.payload;
+        },
+        decrement : (state)=>{
+            state.count -=1
+        },
     }
 })
+
+export const {increment,decrement , incrementbyValue} = counterSlice.actions;
+
+export default counterSlice.reducer;
