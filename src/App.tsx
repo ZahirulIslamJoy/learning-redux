83721eda778848fe82096c7e3714ab119c619113
ZahirulIslamJoy <neloy1613@gmail.com>
2024-08-22 20:42:38 +0600

    configure the store

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 function App() {
   
-
-
   return (
     <>
       <div className="flex justify-center items-center h-screen">
-        <div className="flex border border-purple-300 p-6 bg-slate-200 ">
+        <div className="flex gap-3 border border-purple-300 p-6 bg-slate-200 ">
           <button  className="px-3 py-2 p-10 bg-green-600 rounded-xl">
             Increment
           </button>
