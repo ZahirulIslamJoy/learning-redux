741e4bd3e339f0fa19fa3f6d1fb89ce9f1559500
ZahirulIslamJoy <neloy1613@gmail.com>
2024-08-22 21:06:35 +0600

    completed the counter app using redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
+
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "./redux/features/store";
+import { decrement, increment, incrementBy5 } from "./redux/features/counter/counterSlice";
+
 function App() {
   
-  
+  const {value}= useSelector((state : RootState)=>state.counter)
+  const dispatch = useDispatch();
 
 
   return (
     <>
       <div className="flex justify-center items-center h-screen">
         <div className="flex gap-3 border border-purple-300 p-6 bg-slate-200 ">
-          <button  className="px-3 py-2 p-10 bg-green-600 rounded-xl">
+          <button onClick={()=>dispatch(increment())} className="px-3 py-2 p-10 bg-green-600 rounded-xl">
             Increment
           </button>
-          <button className="px-3 py-2 p-10 bg-green-600 rounded-xl">
+          <button onClick={()=>dispatch(incrementBy5(5))} className="px-3 py-2 p-10 bg-green-600 rounded-xl">
             Increment By 5
           </button>
-          <h1 className="text-3xl mx-10">0</h1>
-          <button  className="px-3 py-2 p-10 bg-red-600 rounded-xl">
+          <h1 className="text-3xl mx-10">{value}</h1>
+          <button onClick={()=>dispatch(decrement())}  className="px-3 py-2 p-10 bg-red-600 rounded-xl">
             Decrement
           </button>
         </div>
